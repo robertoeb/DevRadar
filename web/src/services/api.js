56a7a4686cad42c3c26ac57bd2324f2dea10aacb
Roberto Eustaquio <robertoeb.jr@gmail.com>
2020-02-02 19:48:32 -0300

    Change env variable

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.URL || "http://localhost:3333"}`
+  baseURL: `${process.env.PUBLIC_URL || "http://localhost:3333"}`
 });
 
 export default api;
