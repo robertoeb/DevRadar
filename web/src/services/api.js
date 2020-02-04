2b221480a4d12ebc7773bfb0d88db492f3022939
Roberto Eustaquio <robertoeb.jr@gmail.com>
2020-02-03 22:45:16 -0300

    Fix

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(process.env);
+
 const api = axios.create({
-  baseURL: process.env.URL
+  baseURL: process.env.REACT_APP_API_ENDPOINT
 });
 
 export default api;
